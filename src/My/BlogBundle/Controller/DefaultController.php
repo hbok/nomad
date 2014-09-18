3861010d2d47b19830e458c084eb99c54e3bddb0
岡 広樹 <ithills@oka-no-MacBook-Air.local>
2014-09-18 18:12:06 +0900

    initial commit

---
+++ src/My/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace My\BlogBundle\Controller;
+
+use My\BlogBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use My\BlogBundle\Form\PostType;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $posts = $em->getRepository('MyBlogBundle:Post')->findAll();
+        return $this->render('MyBlogBundle:Default:index.html.twig', array('posts' => $posts));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        return $this->render('MyBlogBundle:Default:show.html.twig', array('post' => $post));
+    }
+
+     public function newAction()
+    {
+        // フォームのビルド
+        //$form = $this->createFormBuilder(new Post())  // ここでPostクラスを使うため、ファイルの先頭あたりにuseを追加していることに注意
+        //    ->add('title')
+        //    ->add('body')
+        //    ->getForm();
+        $form = $this->createForm(new PostType(), new Post());
+
+        // バリデーション
+        $request = $this->getRequest();
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                // エンティティを永続化
+                $post = $form->getData();
+                //$post->setCreatedAt(new \DateTime());
+                //$post->setUpdatedAt(new \DateTime());
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($post);
+                $em->flush();
+                $this->get('session')->getFlashBag()->set('my_blog', '記事を追加しました');
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // 描画
+        return $this->render('MyBlogBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+        $em->remove($post);
+        $em->flush();
+        $this->get('session')->getFlashBag()->set('my_blog', '記事を削除しました');
+        return $this->redirect($this->generateUrl('blog_index'));
+    }
+
+    public function editAction($id)
+    {
+        // DBから取得
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+
+        // フォームのビルド
+        //$form = $this->createFormBuilder($post)
+        //    ->add('title')
+        //    ->add('body')
+        //    ->getForm();
+        $form = $this->createForm(new PostType(), $post);
+
+        // バリデーション
+        $request = $this->getRequest();
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                // 更新されたエンティティをデータベースに保存
+                $post = $form->getData();
+                //$post->setUpdatedAt(new \DateTime());
+                $em->flush();
+                $this->get('session')->getFlashBag()->set('my_blog', '記事を編集しました');
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // 描画
+        return $this->render('MyBlogBundle:Default:edit.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }
+}
---
+++ src/My/BlogBundle/Entity/Post.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace My\BlogBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Post
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="title", type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     * min = 2,
+     * max = 50
+     *)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(name="body", type="text")
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     * min = 10
+     * )
+     */
+    protected $body;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime")
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Post
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Post
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Post
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * set values bedore persist
+     *
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * set values bedore update
+     *
+     * @ORM\PreUpdate
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+}
